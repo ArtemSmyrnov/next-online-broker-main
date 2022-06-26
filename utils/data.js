63b4642316f86bdec0303deafea6801a95ee8f64
Artem Smyrnov <artem.smyrnov@sojournsolutions.com>
2022-06-26 18:06:42 +0200

    next

--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Admin',
+      email: 'admin@gmail.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Test',
+      email: 'test@gmail.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       slug: 'btc',
